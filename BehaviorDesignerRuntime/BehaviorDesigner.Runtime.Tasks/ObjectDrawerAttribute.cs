6827f2c28e45d6443ea9e2fbbdd864bcc39c39e4
372285834 <372285834.qq.com>
2019-04-30 23:54:28 +0800

    1

--- BehaviorDesignerRuntime/BehaviorDesigner.Runtime.Tasks/ObjectDrawerAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BehaviorDesigner.Runtime.Tasks
-{
-	[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
-	public abstract class ObjectDrawerAttribute : Attribute
-	{
-	}
-}
--- BehaviorDesignerRuntime/BehaviorDesigner.Runtime.Tasks/SkipErrorCheckAttribute.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace BehaviorDesigner.Runtime.Tasks
-{
-	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-	public class SkipErrorCheckAttribute : Attribute
-	{
-	}
-}
